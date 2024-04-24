3f61d30fa9a20fdaacc0d7c1904f27adb543b348
sbdarjun <arzoonsubedi123@gmail.com>
2024-04-24 15:19:30 -0500

    comment unused script on cypress yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "17g3vg",
   e2e: {
+    projectId: "17g3vg",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
