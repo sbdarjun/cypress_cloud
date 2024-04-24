8b9b77ba2e1032260f1f4e15845ad6a44a646bea
sbdarjun <arzoonsubedi123@gmail.com>
2024-04-24 12:28:40 -0500

    Cypress Cloud Project Setup

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "17g3vg",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
